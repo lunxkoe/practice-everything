07bf93778da7e6d1327e67e06a6613908438fda3
lunxkoe <lunxkoe@gmail.com>
2026-07-12 15:47:32 +0900

    chore: project-init

---
+++ practice-my-project-initial-settings-v1/src/main/java/lunxkoe/practice/PracticeMyProjectInitialSettingsV1Application.java
@@ -0,0 +1,13 @@
+package lunxkoe.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMyProjectInitialSettingsV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeMyProjectInitialSettingsV1Application.class, args);
+    }
+
+}
